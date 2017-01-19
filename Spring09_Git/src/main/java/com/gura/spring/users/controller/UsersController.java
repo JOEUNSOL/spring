fdcc57aad45735a87b6e2d05c0eacdac865c8c4a
eunsol <dmsthf0904@gmail.com>
2017-01-19 16:47:16 +0900

    회원가입 로그인 로그아웃 기능을 구현하기위한 기본 구성완료 template 구성함

---
+++ Spring09_Git/src/main/java/com/gura/spring/users/controller/UsersController.java
@@ -0,0 +1,24 @@
+package com.gura.spring.users.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.gura.spring.users.service.UsersService;
+
+// component 스캔시  bean 이 되고 또한 컨트롤러역활을 할수 있도록
+@Controller
+public class UsersController {
+	//의존 객체 주입 되도록
+	@Autowired
+	private UsersService usersService;
+	
+	@RequestMapping()
+	public ModelAndView insert(){
+		
+		return null;
+	}
+	
+	
+}
