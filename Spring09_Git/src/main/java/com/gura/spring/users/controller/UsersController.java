e626a0ee01c14c61de81efca0bd6efd59889a554
eunsol <dmsthf0904@gmail.com>
2017-01-20 11:26:14 +0900

    회원 가입 기능 구현됨

--- Spring09_Git/src/main/java/com/gura/spring/users/controller/UsersController.java
+++ Spring09_Git/src/main/java/com/gura/spring/users/controller/UsersController.java
@@ -2,12 +2,17 @@ package com.gura.spring.users.controller;
 
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.gura.spring.users.dto.UsersDto;
 import com.gura.spring.users.service.UsersService;
 
 // component 스켄시 bean 이되고 또한 컨트롤러 역활을 할수 있도록
@@ -30,6 +35,17 @@ public class UsersController {
 	public String signupForm(){
 		
 		return "users/signup_form";
+	}
+	@RequestMapping("/users/signup")
+	public ModelAndView signin(HttpServletRequest request, @ModelAttribute UsersDto dto){
+		//서비스를 이용해서 회원정보를 저장한다.
+		usersService.insert(dto);
+		
+		ModelAndView mview =new ModelAndView();
+		mview.addObject("msg",dto.getId()+"회원님 가입 되었습니다.");
+		mview.addObject("redirectUri",request.getContextPath());
+		mview.setViewName("users/alert");
+		return mview;
 	}
 }
 
